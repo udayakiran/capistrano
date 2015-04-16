d43078639e588395749b7a7a758e781c63307b77
Udaya Kiran <udaykiran.vit@gmail.com>
2015-04-16 16:53:12 +0530

    Adding svn support as scm

--- lib/capistrano/svn.rb
+++ lib/capistrano/svn.rb
@@ -1,9 +1,9 @@
-load File.expand_path("../tasks/svn.rake", __FILE__)
+oad File.expand_path("../tasks/svn.rake", __FILE__)
 
 require 'capistrano/scm'
 
 class Capistrano::Svn < Capistrano::SCM
-  
+
   # execute svn in context with arguments
   def svn(*args)
     args.unshift(:svn)
@@ -15,24 +15,28 @@ class Capistrano::Svn < Capistrano::SCM
       test! " [ -d #{repo_path}/.svn ] "
     end
 
+
     def check
-      test! :svn, :info, repo_url
+      test! :svn, :info, repo_url,  "--no-auth-cache --username #{fetch(:scm_username)} --password #{fetch(:scm_password)}"
     end
 
     def clone
-      svn :checkout, repo_url, repo_path
+      svn :checkout, repo_url, repo_path, "--no-auth-cache --username #{fetch(:scm_username)} --password #{fetch(:scm_password)}"
     end
 
     def update
-      svn :update
+      svn :update, "--no-auth-cache --username #{fetch(:scm_username)} --password #{fetch(:scm_password)}"
     end
 
     def release
-      svn :export, '--force', '.', release_path
+      svn :export, '.', release_path
     end
-
+    
     def fetch_revision
-      context.capture(:svnversion, repo_path)
+      context.capture(:svn," log --no-auth-cache --username #{fetch(:scm_username)}", "--password \
+#{fetch(:scm_password)}"," -r HEAD -q | tail -n 2 | head -n 1 | sed \"s/\ \|.*/''/\"")
     end
   end
 end
+
+
